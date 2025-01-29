e8948e072efbaa0603580920da05eb5684746dda
ashishhiranwal007 <ashishheranwal007@gmail.com>
2025-01-29 12:47:09 +0530

    non-repeating question done

--- Queue/QueueB.java
+++ Queue/QueueB.java
@@ -1,7 +1,25 @@
 package Queue;
+import java.util.*;
 public class QueueB {
      public static void non_repreating(String s){
-        HashMap<String,Integer> map = new HashMap<>();
+        int freq[] =new int[26];
+        Queue<Character> q = new LinkedList<>();
+        for(int i =0;i<s.length();i++){
+            char ch = s.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+            while(!q.isEmpty() && freq[q.peek()-'a']>1){
+                q.remove();
+            }
+            if(q.isEmpty()){
+                System.out.print("-1"+" ");
+            }
+            else{
+                System.out.print(q.peek()+" ");
+            }
+        }
+        System.err.println();
+
      }
     public static void main(String[] args){
         String  s = "aabbcssd";
