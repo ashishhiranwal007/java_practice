92d9e65b0c8c360de2593063eb60654627941839
ashishhiranwal007 <ashishheranwal007@gmail.com>
2025-02-04 14:46:27 +0530

    added activity selection problem

--- Greedy/Greedy.java
+++ Greedy/Greedy.java
@@ -1,3 +1,21 @@
+package Greedy;
+
+import java.util.*;
 public class Greedy {
-    
+     public static void Activity_selection_problem(int start[],int end []){
+        int maxacitivity = 0;
+        int lastend = end[0];
+        ArrayList <Integer> list = new ArrayList<>();
+        list.add(0);
+        maxacitivity=1;
+        for(int i =0;i<end.length;i++){
+         if(start[i]>=end[i]){
+            list.add(i);maxacitivity++;lastend = end[i];
+         }
+        }
+        for(int i  =0;i<list.size();i++){
+         System.out.print("A"+list.get(i)+" ");
+        }
+        System.err.println();
+     }
 }
